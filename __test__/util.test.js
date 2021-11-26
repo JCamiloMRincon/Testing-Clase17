7fc70640fe93856475b4f6e17a8e5a3e53530d84
JCamiloMRincon <camilomortigo@gmail.com>
2021-11-25 21:01:34 -0500

    Second Commit

--- __test__/util.test.js
+++ __test__/util.test.js
@@ -1,58 +1,42 @@
 const {generateText, validateInput, createElement} = require('../util');
 
-test('inputOK', () => {
-    const data = generateText('Miercoles', 24) //Lógica
-    expect(data).toBe('Miercoles (24 years old)');
+describe('Tests de generateText', () => {
+    test('inputOK', () => {
+        const data = generateText('Miercoles', 24)
+        expect(data).toBe('Miercoles (24 years old)');
+    })
+    
+    test('inputNull', () => {
+        const data = generateText(null, null)
+        expect(data).toBe('null (null years old)');
+    })
+
+    test('inputUndefined', () => {
+        const data = generateText(undefined, undefined)
+        expect(data).toBe('undefined (undefined years old)');
+    })
 })
 
-test('inputOK2', () => {
-    const data2 = generateText('Miercoles', 24) //Lógica
-    expect(data2).toMatch(/Miercoles/);
-})
+describe('Tests de validateInput', () => {
+    test('firstValidateInput_False', () => {
+        const validate = validateInput(null)
+        expect(validate).toBeFalsy;
+    })
+    
+    test('secondValidateInput_False', () => {
+        const validate = validateInput('   ', 'Hello World')
+        expect(validate).toBeFalsy;
+    })
 
-test('inputOK3', () => {
-    const data2 = generateText() //Lógica
-    expect(data2).toBe('undefined (undefined years old)');
-})
+    test('thirdValidateInput_False', () => {
+        const validate = validateInput(NaN, ' ', 'Hello World')
+        expect(validate).toBeFalsy;
+    })
 
-test('inputOK3', () => {
-    const data3 = generateText('Miercoles', 24) //Lógica
-    expect(typeof(data3)).toBe('string');
-})
-
-test('validacion1', () => {
-    const valid = validateInput('arroz') //Lógica
-    expect(valid).toBeFalsy; // espero TRUE o FALSE
-})
-
-test('validacion3', () => {
-    const valid = validateInput(null) //Lógica
-    expect(valid).toBeFalsy(); // espero TRUE o FALSE
-})
-
-test('validacion4', () => {
-    const valid = validateInput('  hola  ') //Lógica
-    expect(valid).toBeFalsy; // espero TRUE o FALSE
-})
-
-test('validacion5', () => {
-    const valid = validateInput(NaN, false, false) //Lógica
-    expect(valid).toBeFalsy(); // espero TRUE o FALSE
-})
-
-test('validacion6', () => {
-    const valid = validateInput(NaN, false, true) //Lógica
-    expect(valid).toBeFalsy(); // espero TRUE o FALSE
-})
-
-test('validacion7', () => {
-    const valid = validateInput(NaN, true, false) //Lógica
-    expect(valid).toBeFalsy(); // espero TRUE o FALSE
-})
-
-test('validacion8', () => {
-    const valid = validateInput(NaN, true, true) //Lógica
-    expect(valid).toBeFalsy(); // espero TRUE o FALSE
+    test('fourthValidateInput_True', () => {
+        const validate = validateInput('Hello', 'World', ' ')
+        expect(validate).toBeTruthy;
+    })
 })
 
 
